1a4690ccf9f874dbb518992a6499d6a05d819d39
SyrymNurbaturov <mr.nurbaturov@mail.ru>
2023-03-13 11:33:34 +0600

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack =  require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { NetlifyPlugin } = require("netlify-webpack-plugin");
 const production = process.env.NODE_ENV === 'production';
 
@@ -11,6 +13,36 @@ module.exports = {
         filename: production ? '[main].[contenthash].js' : '[main].js',
         publicPath: '/'
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    plugins: [
+                        [
+                            'import',
+                            {libraryName: "antd", style: true},
+                            'antd',
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    production ? MiniCssExtractPlugin.loader : 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: !production
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new NetlifyPlugin({
             redirects: [
@@ -20,6 +52,15 @@ module.exports = {
                     status: 200
                 }
             ]
+        }),
+        new HtmlWebpackPlugin({
+            title: "My blog",
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico"
+        }),
+        new MiniCssExtractPlugin({
+            filename: production ? '[main].[contenthash].css' : '[main].css'
         })
-    ]
+    ],
+    mode: production ? 'production' : 'development'
 }
